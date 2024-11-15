05dab8e4d562c635415492d998c6ee4a77148d6c
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-11-15 13:47:10 +0530

    12

---
+++ Practice C++/2.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    // Remove the underscores and complete the code
+    int n=1;
+    while (n<=5) 
+    {
+        cout << n <<"-" <<n*n << endl;
+        n++;
+    }
+}
+
---
+++ Practice C++/5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int a = 4;
+  int b = 1;
+  while ( a != b )
+   {
+    cout << b << " ";
+    b = b+1;
+  }
+  
+}
+
+
