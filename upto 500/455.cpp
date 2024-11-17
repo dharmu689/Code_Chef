6d87a68b387380f090e26fbe5c07376c00eb3c3e
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-11-17 17:46:59 +0530

    17-11-24

---
+++ upto 500/455.cpp
@@ -0,0 +1,27 @@
+//Question - https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/FLOW006?tab=statement
+//Solution - https://www.codechef.com/viewsolution/1107558115
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() 
+{
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, s = 0;
+        cin >> n;
+        while (n != 0)
+        {
+            s = s + (n % 10);
+            n = (n / 10);
+            
+        }
+        cout << s << endl;
+        
+    }
+
+}
