7da813ac23dc84e9647d8a437687138a42d5e26a
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-11-19 17:21:50 +0530

    19-11-24

---
+++ upto 500/461.cpp
@@ -0,0 +1,24 @@
+//Question - https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/FLOW004?tab=hints
+//Solution - https://www.codechef.com/viewsolution/1108270892
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,l;
+	    cin>>n;
+	    l=n%10;
+	    while(n>9)
+	    {
+	        n=n/10;
+	    }
+	    cout<<l+n<<endl;
+	    
+	}
+
+}
