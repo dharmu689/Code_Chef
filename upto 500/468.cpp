fad06474005b52dd99492331fc592ac6ae7c7bf9
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-11-20 13:37:53 +0530

    Create 468.cpp

---
+++ upto 500/468.cpp
@@ -0,0 +1,27 @@
+//Question - https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/ASSIGNMNT
+//Solution - https://www.codechef.com/viewsolution/1108537203
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, y, z;
+        int m = 24 * 60;
+        cin >> x >> y >> z;
+        if ((x * y) <= (z * m))
+        {
+            cout << "yes" << endl;
+        }
+        else
+        {
+            cout << "no" << endl;
+        }
+    }
+
+}
