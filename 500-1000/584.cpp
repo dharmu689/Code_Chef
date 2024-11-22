b65c2d8aae667ff6b4911ce7e3d155b2d218ba54
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-11-22 13:07:19 +0530

    Create 584.cpp

---
+++ 500-1000/584.cpp
@@ -0,0 +1,26 @@
+//Question - https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/ACTEMP
+//Solution - https://www.codechef.com/viewsolution/1109012669
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if ((a <= b) && (b >= c))
+        {
+            cout << "yes" << endl;
+        }
+        else
+        {
+            cout << "no" << endl;
+        }
+    }
+
+}
