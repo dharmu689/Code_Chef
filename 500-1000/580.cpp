d630922f71d380e0e922377ba51b590d476fa8b1
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-11-22 13:07:13 +0530

    Create 580.cpp

---
+++ 500-1000/580.cpp
@@ -0,0 +1,31 @@
+//Question - https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/TODOLIST?tab=statement
+//Solution - https://www.codechef.com/viewsolution/1108978235
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, c = 0;
+        cin >> n;
+        while (n--)
+        {
+            int d;
+            cin>>d;
+            if(d>=1000)
+            {
+                c++;
+            }
+        }
+        cout << c << endl;
+
+
+    }
+    return 0;
+
+}
