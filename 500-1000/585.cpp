487b8b798add2fbb648af39689d537df3eb29e75
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-11-22 13:07:24 +0530

    Create 585.cpp

---
+++ 500-1000/585.cpp
@@ -0,0 +1,26 @@
+//Question - https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/NEARESTEXIT
+//Solution - https://www.codechef.com/viewsolution/1109014814
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x;
+        cin >> x;
+        if ((1 <= x) && (50 >= x))
+        {
+            cout << "left" << endl;
+        }
+        else
+        {
+            cout << "right" << endl;
+        }
+    }
+
+}
