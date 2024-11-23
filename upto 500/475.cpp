2acc90795e02da21a824a7f76ad0cd1da8c0141f
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-11-23 18:01:20 +0530

    22-11-24

---
+++ upto 500/475.cpp
@@ -0,0 +1,26 @@
+//Question -  https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/AIRLINES
+//Solution - https://www.codechef.com/viewsolution/1109272622
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, y, z;
+        cin >> x >> y >> z;
+        if ((x * 10) >= y)
+        {
+            cout << y * z << endl;
+        }
+        else
+        {
+            cout << x * 10 * z << endl;
+        }
+    }
+
+}
