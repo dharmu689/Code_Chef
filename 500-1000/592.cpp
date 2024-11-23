993a2e159174c900983020e3e2d5b445f3d43e7a
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-11-23 18:00:53 +0530

    Create 592.cpp

---
+++ 500-1000/592.cpp
@@ -0,0 +1,31 @@
+//Question - https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/SST?tab=statement
+//Solution - https://www.codechef.com/viewsolution/1109295938
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        if ((10 * a) > (5 * b))
+        {
+            cout << "first" << endl;
+        }
+        else if ((10 * a) < (5 * b))
+        {
+            cout << "second" << endl;
+        }
+        else
+        {
+            cout << "any" << endl;
+        }
+
+    }
+
+}
