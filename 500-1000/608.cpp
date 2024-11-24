2b87081bb48bafb353538ab0310ac26fa3c4e01c
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-11-24 22:41:21 +0530

    Create 608.cpp

---
+++ 500-1000/608.cpp
@@ -0,0 +1,31 @@
+//Question - https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/MINCARS?tab=statement
+//Solution - https://www.codechef.com/viewsolution/1109737641
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+        // your code goes here
+        int t;
+        cin >> t;
+        while (t--)
+        {
+            int n;
+            cin >> n;
+
+            if ((n % 4) == 0)
+            {
+                cout << n / 4 << endl;
+            }
+            else if (n < 4)
+            {
+                cout << "1" << endl;
+            }
+            else
+            {
+                cout << (n / 4) + 1 << endl;
+            }
+
+        }
+}
