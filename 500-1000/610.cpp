706778d63655313d389daf73bb89d52d1a6c5383
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-11-25 16:24:18 +0530

    25-11-24

---
+++ 500-1000/610.cpp
@@ -0,0 +1,28 @@
+//Question - https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/CHEFSCORE
+//Solution - https://www.codechef.com/viewsolution/1109829806
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, x, y;
+        cin >> n >> x >> y;
+
+        if ((y % x) == 0)
+        {
+            cout << "Yes" << endl;
+        }
+        else
+        {
+            cout << "No" << endl;
+        }
+    }
+
+}
