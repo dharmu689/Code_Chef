1e3b6143d0d9639b7a2ac4da9be97ea75d9fa9a1
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-11-30 21:56:33 +0530

    30-11-24

---
+++ upto 500/847_miami_GP.cpp
@@ -0,0 +1,27 @@
+//Question - https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/F1RULE
+//Solution - https://www.codechef.com/viewsolution/1111175867
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, y;
+        cin >> x >> y;
+        if ((x * 1.07) < y)
+        {
+            cout << "no" << endl;
+        }
+        else
+        {
+            cout << "yes" << endl;
+        }
+    }
+
+}
