5e6fbcb21cc7eb700156591a5fc1da8d6255df24
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-12-01 09:11:36 +0530

    Update 421.cpp

---
+++ upto 500/421.cpp
@@ -0,0 +1,21 @@
+//Question - https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/FLOW002?tab=statement
+//Solution - https://www.codechef.com/viewsolution/1106270420
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << a % b << endl;
+    }
+
+}
+
