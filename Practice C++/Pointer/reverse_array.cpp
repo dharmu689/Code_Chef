a1d308dfdf60bcf109eeeac4350d8ff9e8cada66
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-12-05 10:26:04 +0530

    Create reverse_array.cpp

---
+++ Practice C++/Pointer/reverse_array.cpp
@@ -0,0 +1,40 @@
+//Question - https://www.codechef.com/practice/course/cpp/LPCPAS13B/problems/CPPPTR2?tab=statement
+//Solution - https://www.codechef.com/viewsolution/1112415743
+
+#include <iostream>
+
+using namespace std;
+
+// Function to reverse the elements of an array using pointers
+void reverseArray(int * arr, int size) {
+    // Complete this function
+    int* start = arr;
+    int* end = arr + size - 1;
+    while (start < end)
+    {
+        int temp = *start;
+        * start = *end;
+        * end = temp;
+        start++;
+        end--;
+    }
+
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Call the function to reverse the array
+    reverseArray(arr, n);
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
