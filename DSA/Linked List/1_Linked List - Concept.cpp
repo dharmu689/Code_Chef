adabf65227e2ad9fdc91329d3e14c43605165ddc
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-12-08 21:49:30 +0530

    Create 1_Linked List - Concept.cpp

---
+++ DSA/Linked List/1_Linked List - Concept.cpp
@@ -0,0 +1,21 @@
+//Question - https://www.codechef.com/learn/course/linked-lists/LINKEDLIST01/problems/LINK01P01
+//Solution - https://www.codechef.com/viewsolution/1113147199
+
+
+#include <iostream>
+using namespace std;
+
+// Create a struct with two values
+struct Node {
+    int value;
+    Node* next;
+    
+    // Constructor to initialize the node with a given value
+    Node(int val): value(val), next(nullptr) {}
+};
+
+int main() {
+    // Creating head of the Linked list
+    Node* head = new Node(1);
+    cout << "The value at head is " << head -> value << '\n';
+}
