bc1a1065856d6ba15882934e82cb0ce85c366fc3
Dharmu kumar <dharmukumar1622003@gmail.com>
2025-01-30 15:18:16 +0530

    Create 641_Flip the cards.cpp

---
+++ 500-1000/641_Flip the cards.cpp
@@ -0,0 +1,47 @@
+//Quesyion - https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/FLIPCARDS
+//Solution - https://www.codechef.com/viewsolution/1126950603
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, x;
+        cin >> n >> x;
+
+        int facedown = n-x;
+        
+        if(n == x)
+        {
+            cout<<"0"<<endl;
+        }
+        
+         else if(n == facedown)
+        {
+            cout<<"0"<<endl;
+        }
+        
+        else if (x < facedown || x>facedown || x==facedown)
+        {
+            if(x<facedown)
+            {
+                cout<<x<<endl;
+            }
+            else if (x>facedown)
+            {
+                cout<<facedown<<endl;
+            }
+            else
+            {
+                cout<<facedown<<endl;
+            }
+        }
+
+    }
+
+}
