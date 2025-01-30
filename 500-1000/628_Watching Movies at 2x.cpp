800e51c25a1d5d250eed1971561f25e144c29c5d
Dharmu kumar <dharmukumar1622003@gmail.com>
2025-01-30 15:18:24 +0530

    Create 628_Watching Movies at 2x.cpp

---
+++ 500-1000/628_Watching Movies at 2x.cpp
@@ -0,0 +1,16 @@
+
+//Question - https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/MOVIE2X
+//Solution - https://www.codechef.com/viewsolution/1126825857
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() 
+{
+    // your code goes here
+    int x, y;
+    cin >> x >> y;
+    cout << (x - y) + (y / 2);
+
+}
