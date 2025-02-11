551cfade830a1ddc34924ca86dab6e4c000a90b4
Dharmu kumar <dharmukumar1622003@gmail.com>
2025-02-11 11:35:06 +0530

    Create 668_Finding Square Roots.cpp

---
+++ 500-1000/668_Finding Square Roots.cpp
@@ -0,0 +1,18 @@
+//Question - https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/FSQRT?tab=statement
+//Solution - https://www.codechef.com/viewsolution/1131353057
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	     cout << int(sqrt(n)) << endl;
+	}
+
+}
