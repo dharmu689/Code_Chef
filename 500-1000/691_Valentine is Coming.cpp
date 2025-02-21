86617002dd6c1e6dacd1be03c32b2c388b5f7f3c
Dharmu kumar <dharmukumar1622003@gmail.com>
2025-02-21 20:07:19 +0530

    Create 691_Valentine is Coming.cpp

---
+++ 500-1000/691_Valentine is Coming.cpp
@@ -0,0 +1,21 @@
+// Question - https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/VALENTINE
+
+// Solution - https://www.codechef.com/viewsolution/1134100170
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, y;
+        cin >> x >> y;
+        cout << x / y << endl;
+    }
+    return 0;
+}
