18f35cee9a5e8a1a44965ec085a116803e2846b0
Dharmu kumar <dharmukumar1622003@gmail.com>
2025-03-01 23:07:51 +0530

    Create 702_Chef and his Apps.cpp

---
+++ 500-1000/702_Chef and his Apps.cpp
@@ -0,0 +1,30 @@
+//Question - https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/CHEFAPPS
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int s, x, y, z;
+        cin >> s >> x >> y >> z;
+        int a = s - (x + y);
+        if (a >= z)
+        {
+            cout << "0" << endl;
+        }
+        else if (a + x >= z || a + y >= z)
+        {
+            cout << "1" << endl;
+        }
+        else
+        {
+            cout << "2" << endl;
+        }
+    }
+
+}
