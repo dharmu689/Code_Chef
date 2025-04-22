32bd9ed13aab3d0e54fbcb283639bb647f12f80a
Dharmu kumar <dharmukumar1622003@gmail.com>
2025-04-22 10:14:12 +0530

    lkkhnbhjfvtc

---
+++ Contests/Starters 163 (Rated)/212_Chef and Socks.cpp
@@ -0,0 +1,19 @@
+// Question - https://www.codechef.com/problems/CHEFSOCKS?tab=statement
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // your code goes here
+    int a, x, y;
+    cin >> a >> x >> y;
+    if ((x + y) >= a)
+    {
+        cout << "yes" << endl;
+    }
+    else
+    {
+        cout << "no";
+    }
+}
