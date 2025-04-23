a3b921754ace0b8e3c9d39436a9d0709f1bed40e
Dharmu kumar <dharmukumar1622003@gmail.com>
2025-04-24 00:14:58 +0530

    Create 467_New-Pro Coder.cpp

---
+++ Contests/Starters 164 (Rated)/467_New-Pro Coder.cpp
@@ -0,0 +1,21 @@
+// Question - https://www.codechef.com/problems/SPC2025?tab=statement
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    // your code goes here
+    double n, m;
+    cin >> n >> m;
+    if (m >= (n / 2))
+    {
+        cout << "NEWBIE";
+    }
+    else
+    {
+        cout << "PRO";
+    }
+    return 0;
+}
