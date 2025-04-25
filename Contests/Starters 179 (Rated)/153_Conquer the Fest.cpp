8e8d011de72f17ccbc4a303bac85b44de4d5e5ef
Dharmu kumar <dharmukumar1622003@gmail.com>
2025-04-25 13:36:55 +0530

    Create 153_Conquer the Fest.cpp

---
+++ Contests/Starters 179 (Rated)/153_Conquer the Fest.cpp
@@ -0,0 +1,20 @@
+// Question - https://www.codechef.com/problems/CLSI?tab=statement
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // your code goes here
+
+    int n, b;
+    cin >> n >> b;
+    if (n >= (b * 10))
+    {
+        cout << "yes";
+    }
+    else
+    {
+        cout << "no";
+    }
+}
