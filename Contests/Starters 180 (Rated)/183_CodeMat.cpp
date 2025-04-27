0f013511acf25cb8a9e0fcef9726ec764adf53d6
Dharmu kumar <dharmukumar1622003@gmail.com>
2025-04-27 18:40:05 +0530

    Create 183_CodeMat.cpp

---
+++ Contests/Starters 180 (Rated)/183_CodeMat.cpp
@@ -0,0 +1,20 @@
+// Question -https://www.codechef.com/problems/CDMT?tab=statement
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    // your code goes here
+    int x, y;
+    cin >> x >> y;
+    if (x > y)
+    {
+        cout << "no";
+    }
+    else
+    {
+        cout << "yes";
+    }
+}
